f948b9ed690694a4c078bfba088235c0ca16aa0c
Emil <crysis363@gmail.com>
2019-12-02 17:03:37 +0300

    Add files via upload

--- oracle_con/oracle_con.py
+++ oracle_con/oracle_con.py
@@ -1,35 +1,45 @@
 import cx_Oracle
 import os
+import time
+from tqdm import * 
+
+user_name = "TEST"
 
 con = cx_Oracle.connect('admin/84218421@52.14.136.177/orcl')
 curs = con.cursor()
 print (con.version)
 
-file = open(os.path.join(os.path.dirname(__file__),'ot_create_user.sql'), 'r')
-full_sql = file.read()
-sql_commands = full_sql.split(';')
+def create_user():
+    file = open(os.path.join(os.path.dirname(__file__), 'ot_create_user.sql'), 'rt')   
+    data = file.read()
+    data = data.replace('OT', user_name)
+    sql_commands = data.replace('\n', '').split(';')[:-1]
+    file.close()
 
-for sql_command in sql_commands:
-    curs.execute(sql_command)
+    for sql_command in tqdm(sql_commands):
+        curs.execute(sql_command)
 
 def create_tables():
-    file = open(os.path.join(os.path.dirname(__file__),'ot_schema.sql'), 'r')
-    full_sql = file.read()
-    sql_commands = full_sql.split(';')
+    file = open(os.path.join(os.path.dirname(__file__), 'ot_schema.sql'), 'rt')      
+    data = file.read()
+    data = data.replace('OT.', user_name + '.')
+    sql_commands = data.replace('\n', '').split(';')[:-1]
+    file.close()
 
-    for sql_command in sql_commands:
+    for sql_command in tqdm(sql_commands):
         curs.execute(sql_command)
-
 
 def insert_values():
-    file = open(os.path.join(os.path.dirname(__file__), 'ot_data.sql'), 'r')
-    full_sql = file.read()
-    sql_commands = full_sql.split(';')
+    file = open(os.path.join(os.path.dirname(__file__), 'ot_data.sql'), 'rt')      
+    data = file.read()
+    data = data.replace('OT.', user_name + '.')
+    sql_commands = data.replace('\n', '').split(';')[:-1]
+    file.close()
 
-    for sql_command in sql_commands:
+    for sql_command in tqdm(sql_commands):
         curs.execute(sql_command)
-        print(sql_command)
 
+create_user()
 create_tables()
 insert_values()
 con.close()
